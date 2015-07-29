edee8c5c2d83f4306cb8247b50fd409419d122ed
zhuwei05 <zhuwei5e@qq.com>
2015-07-29 20:21:46 +0800

    get_intersection_list

--- projecteuler/recursive.erl
+++ projecteuler/recursive.erl
@@ -16,6 +16,9 @@
 -export([sum_list/1
         ,fib/1
         ,main/1
+        ,get_intersection_list/3
+        ,pred1/2
+        ,get_intersection_list2/2
         ]).
 
 sum_list(T) when is_integer(T) ->
@@ -61,6 +64,27 @@ fib(N, Acc1, Acc2) ->
 %% fib(N, Acc1, Acc2) ->
 %%   fib(N-1, Acc2, Acc1 + Acc2).
 
+pred1(_Elem, []) ->
+  'false';
+pred1(Elem, [H|T]) ->
+  case Elem =:= H of
+    'true' -> 'true';
+    'false' -> pred1(Elem, T)
+  end.
+
+get_intersection_list(List1, List2, Fun) ->
+  List3 = lists:filter(fun(X) -> Fun(X, List1) end, List2),
+  io:format("List: ~p~n", [List3]).
+
+get_intersection_list2(List1, List2) ->
+  List3 = lists:filter(fun(X) ->
+                        lists:any(fun(T) ->
+                          case T == X of
+                            'true' -> 'true';
+                            'false' -> false end end,
+                        List1) end,
+                      List2),
+  io:format("List: ~p~n", [List3]).
 
 main(_) ->
 %%   code:add_patha("../"),
@@ -69,5 +93,20 @@ main(_) ->
 
   io:format("fib1 : ~p, fib2: ~p~n", [fib(1), fib(2)]),
   io:format("fib(10) = 89 = ~p~n", [fib(10)]),
+
+  List1 = ["c7b85d1ccb5b95f5f0b61c3508399f72", "23cc1a758af23b49ff1807f1489ec228", "7ac9c8c75a24672cc493e2016c9afd7c"],
+  List2 = ["23cc1a758af23b49ff1807f1489ec228", "7ac9c8c75a24672cc493e2016c9afd7c"],
+  List3 = ["df"],
+  List4 = ["c7b85d1ccb5b95f5f0b61c3508399f72", "df"],
+
+  io:format("intersection ~p~n", [List1 -- List1 -- List4]),
+
+  get_intersection_list(List1, List2, fun pred1/2),
+  get_intersection_list(List1, List3, fun pred1/2),
+  get_intersection_list(List1, List4, fun pred1/2),
+
+  get_intersection_list2(List1, List2),
+  get_intersection_list2(List1, List3),
+  get_intersection_list2(List1, List4),
 
   ok.
